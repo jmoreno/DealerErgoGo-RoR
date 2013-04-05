997698c459273cc7b6633a1ad208c6cc89feb28a
Javi <jmoreno.zn@gmail.com>
2013-04-06 01:53:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealerErgoGo::Application.config.secret_token = '9c9a83fb3905577b1cacd1686dc095b67783735287ad204186f99eeed0d606b1bd5e4aa44db41c0eb531db271541dec64523caeebfe326630598d34031746185'
